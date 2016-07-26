5a91b81e6a58c966928e7c8f2b46630b001e2847
binh.x.nguyen <binhxn89@gmail.com>
2016-07-26 12:17:10 -0700

    Restructured and modularized gulp tasks, added uglify, buffer and require-dir

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,40 +1,17 @@
 var gulp = require('gulp')
-var sass = require('gulp-ruby-sass')
-var connect = require('gulp-connect')
-var browserify = require('browserify')
-var source = require('vinyl-source-stream')
-
-gulp.task('connect', function () {
-	connect.server({
-		root: 'public',
-		port: 3000
-	})
-})
-
-gulp.task('browserify', function() {
-	// Grabs the app.js file
-    return browserify('./client/app.js')
-    	// bundles it and creates a file called main.js
-        .bundle()
-        .pipe(source('main.js'))
-        // saves it the public/js/ directory
-        .pipe(gulp.dest('./public/js/'));
-})
-
-gulp.task('sass', function() {
-	return sass('sass/style.sass')
-		.pipe(gulp.dest('public/css'))
-})
+var requireDir = require('require-dir')
 
 gulp.task('reset', function() {
-	return
+	return;
 })
+
+// Imports all gulp tasks in /gulp-tasks folder
+requireDir('./gulp-tasks');
 
 gulp.task('watch', function() {
 	gulp.watch('./client/**/*.js', ['browserify'])
 	gulp.watch('./sass/style.sass', ['sass'])
 	gulp.watch('./public/index.html', ['reset'])
-	////
 })
 
-gulp.task('default', ['connect', 'browserify', 'sass', 'watch'])
+gulp.task('default', ['connect', 'browserify', 'sass', 'reset', 'watch'])
--- app/app.js
+++
@@ -1,8 +0,0 @@
-require('angular')
-var MainController = require('./controllers/MainController')
-
-angular.module('app', [
-	require('angular-route'),
-	require('angular-animate')
-	])
-	.controller('MainController', ['$scope', '$http', MainController])
