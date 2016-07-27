c96feda361d84d8b367e64cc1471f51975dcc020
kevinl <kevinliu987@gmail.com>
2016-07-26 22:18:07 -0700

    initial tests for existance of  property and function

--- public/test/unit/comb-test.js
+++ public/test/unit/comb-test.js
@@ -5,7 +5,7 @@ describe('Testing AngularJS Test Suite', function(){
     expect(true).toBe(true);
   });
 
-  describe('Testing AngularJS Controller', function () {
+  describe('Testing AngularJS Controller MainController', function () {
   var scope, ctrl, httpBackend, timeout, rootScope;
   beforeEach(inject(function($controller, $rootScope, $httpBackend, $timeout) {
           rootScope = $rootScope;
@@ -14,10 +14,56 @@ describe('Testing AngularJS Test Suite', function(){
           httpBackend = $httpBackend;
           timeout = $timeout
         }))
-  it('should have filteroptions in the scope', function() {
-    expect(scope.filterOptions).toBeDefined();
-    expect(scope.filterOptions).toBe(false);
-  });
+
+        it('should have scope property ingredients', function() {
+          expect(scope.ingredient).toBeDefined();
+          expect(scope.ingredient).toBe(null);
+        });
+
+        it('should have scope property errors', function() {
+          expect(scope.error).toBeDefined();
+          expect(scope.error).toBe(null);
+        });
+
+        it('should have scope property recipes', function() {
+          expect(scope.recipes).toBeDefined();
+          expect(scope.recipes).toBe(null);
+        });
+
+        it('should have scope property filterOptions', function() {
+          expect(scope.filterOptions).toBeDefined();
+          expect(scope.filterOptions).toBe(false);
+        });
+
+        it('should have scope property textFilter', function() {
+          expect(scope.textFilter).toBeDefined();
+          expect(scope.textFilter).toBe(null);
+        });
+
+        it('should have scope property textFilter', function() {
+          expect(scope.numLimit).toBeDefined();
+          expect(scope.numLimit).toBe(null);
+        });
+
+        it('should have scope function searchRecipe', function() {
+          expect(scope.searchRecipe).toBeDefined();
+          //test functionality of this function
+          // expect(scope.searchRecipe).toBe(null);
+        });
+
+        it('should have scope property textFilter', function() {
+          expect(scope.numLimit).toBeDefined();
+          expect(scope.numLimit).toBe(null);
+        });
+
+        it('should have scope property textFilter', function() {
+          expect(scope.numLimit).toBeDefined();
+          expect(scope.numLimit).toBe(null);
+        });
+
+
+
+
 
 
   })
